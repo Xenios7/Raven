afb944ed8d4f80d410d7e5eb640ab19b759e92d8
Xenios7 <xyerol01@ucy.ac.cy>
2026-05-29 13:39:27 +0300

    feat: implemented broker layer

---
+++ internal/broker/broker.go
@@ -0,0 +1,95 @@
+package broker
+
+import (
+	"hash/fnv"
+
+	"github.com/Xenios7/Raven/internal/store"
+)
+
+type BrokerInterface interface {
+    Publish(content string, topic string, key string, numPartitions int)
+    Consume(topic string, partition int, offset int) ([]store.Message, error)
+}
+
+type Broker struct {
+    store store.StoreInterface //Reference to store struct(interface)
+}
+
+// Constructor
+// Needs store interface (it doesn't know or care if the caller passed in a real *Store, a fake test store, or a disk-based store. It just knows it can call Append and Get on it.)
+// so it can initialize the store that it has
+func NewBroker(s store.StoreInterface) *Broker {
+	return &Broker{
+		store: s,
+	}
+}
+
+func (b *Broker) partition(key string, numPartitions int) int{
+	h := fnv.New32a() //create a hashing machine (convert any string into a number)
+	h.Write([]byte(key)) //feed the key into it (strings must be converted to bytes first)
+	return int(h.Sum32()) % numPartitions //get the result and fit it into the number of partitions using modulo
+
+	/*Hash produces some big number, say 2847291
+	2847291 % 3 = 0
+	Message goes to partition 0
+	*/
+}
+
+/********************************************************************/
+func (b *Broker) Publish(content string, topic string, key string, numPartitions int){
+
+	//Find partition
+	partitionNumber := b.partition(key, numPartitions)
+
+	//Append using store structure from store/
+	b.store.Append(topic, partitionNumber, content)
+
+}
+
+/********************************************************************/
+func (b *Broker) Consume(topic string, partition int, offset int) ([]store.Message, error){
+	return b.store.Get(topic, partition, offset)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
